0a36139b27f17b5f58414370b0886f9537140efa
im-aeo <aeodevelopments@gmail.com>
2025-05-28 19:14:25 -0800

    route fix

--- config/search_routes.php
+++ config/search_routes.php
@@ -1,6 +1,8 @@
 <?php
 
-return [
+use Illuminate\Support\Facades\Auth;
+
+$searchRoutes = [
     [
         'name' => 'Marketplace',
         'keywords' => ['market', 'store', 'shop', 'marketplace', 'buy', 'sell', 'products'],
@@ -18,20 +20,45 @@ return [
         'icon' => 'fas fa-comments',
     ],
     [
+        'name' => 'Discover',
+        'keywords' => ['users', 'discover', 'players', 'online'],
+        'url' => 'route("user.page")',
+        'description' => 'Discover new users.',
+        'type' => 'route',
+        'icon' => 'fad fa-user-group',
+    ],
+];
+
+if (Auth::check() && Auth::user()) {
+    $searchRoutes[] = [[
         'name' => 'Dashboard',
-        'keywords' => ['dashboard', 'account', 'settings', 'profile'],
+        'keywords' => ['dashboard', 'home', 'stats', 'analytics'],
         'url' => 'route("my.dashboard.page")',
         'description' => 'Access your personal dashboard.',
         'type' => 'route',
         'icon' => 'fad fa-tachometer-alt',
-    ],
-    [
-        'name' => 'Discover',
-        'keywords' => ['users', 'discover', 'players', 'online'],
-        'url' => 'route("my.dashboard.page")',
-        'description' => 'Access your personal dashboard.',
+    ], [
+        'name' => 'Money',
+        'keywords' => ['money', 'purchases', 'transactions', 'sales'],
+        'url' => 'route("my.money.page")',
+        'description' => 'Manage your finances and convert your currencies.',
         'type' => 'route',
-        'icon' => 'fad fa-user-group',
-    ],
-    // Add more as needed
-];
+        'icon' => 'fad fa-tachometer-alt',
+    ], [
+        'name' => 'Your Profile',
+        'keywords' => ['profile', '', 'transactions', 'sales'],
+        'url' => 'route("my.money.page")',
+        'description' => 'Manage your finances and convert your currencies.',
+        'type' => 'route',
+        'icon' => 'fad fa-tachometer-alt',
+    ], [
+        'name' => 'Settings',
+        'keywords' => ['account', 'settings', 'profile', 'password', 'appearence', 'language'],
+        'url' => 'route("my.money.page")',
+        'description' => 'Manage your finances and convert your currencies.',
+        'type' => 'route',
+        'icon' => 'fad fa-tachometer-alt',
+    ]];
+};
+
+return $searchRoutes;
